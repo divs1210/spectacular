d2590d01563a2dff043820d830eaa574d247eb48
Divyansh Prakash <divyansh.prakash@yahoo.com>
2018-07-05 01:17:38 +0530

    Bootstrap

--- src/spectacular/core.clj
+++ src/spectacular/core.clj
@@ -2,45 +2,68 @@
   "Spec arbitrary Clojure expressions!"
   (:require [clojure.spec.alpha :as s]))
 
-(defmacro with-check-assert
-  "Puts body in a block where
-  `s/check-asserts` is `true`."
+;; Internals
+;; =========
+(defmacro with-spec-in*
+  "Use `with-spec-in` instead."
+  [sym-spec-map & body]
+  `(do
+     ~@(for [[arg spec] sym-spec-map]
+         `(s/assert ~spec ~arg))
+     ~@body))
+
+(s/def ::sym-spec-map
+  (s/map-of symbol? any?))
+
+
+;; API
+;; ===
+(defmacro with-check-asserts
+  "Puts body in a block in which
+  (s/check-asserts?) is `true`."
   [& body]
   `(with-bindings {s/check-asserts true}
      ~@body))
 
-(defmacro with-spec-in*
-  "Takes a vector of bindings from
+
+(defmacro with-spec-in
+  "Takes a map of bindings from
   symbols to specs,
   inserting respective spec-asserts
   before the body.
 
   NOTE: Asserts work only when
-  `s/check-asserts` is `true`."
-  [args-n-specs & body]
-  `(do
-     ~@(for [[arg spec] (partition 2 args-n-specs)]
-         `(s/assert ~spec ~arg))
-     ~@body))
+  `(s/check-asserts?)` is `true`.
+
+  Check out `with-check-asserts`."
+  [sym-spec-map & body]
+  (with-check-asserts
+    (with-spec-in* {sym-spec-map ::sym-spec-map}
+      `(with-spec-in* sym-spec-map
+         ~@body))))
 
 
-(defmacro with-spec-out*
-  "Inserts a spec-assert on
-  the value resulting from the body.
+(defmacro with-spec-out
+  "Inserts a spec-assert on the value
+  resulting from the evaluation of body.
 
   NOTE: Asserts work only when
-  `s/check-asserts` is `true`."
+  `(s/check-asserts?)` is `true`.
+
+  Check out `with-check-asserts`."
   [spec & body]
   `(s/assert ~spec ~@body))
 
 
-(defmacro with-spec*
-  "Composite of `with-spec-in*`
-  and `with-spec-out*`.
+(defmacro with-spec
+  "Composite of `with-spec-in`
+  and `with-spec-out`.
 
   NOTE: Asserts work only when
-  `s/check-asserts` is `true`."
-  [args-n-specs ret-spec & body]
-  `(with-spec-in ~args-n-specs
-           (with-spec-out ~ret-spec
-                      ~@body)))
+  `(s/check-asserts?)` is `true`.
+
+  Check out `with-check-asserts`."
+  [sym-spec-map ret-spec & body]
+  `(with-spec-in ~sym-spec-map
+     (with-spec-out ~ret-spec
+       ~@body)))
