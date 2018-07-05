b53e9c130d48fa9c00ca8f9bdfa453cd926d3a6b
Divyansh Prakash <divyansh.prakash@yahoo.com>
2018-07-05 12:35:23 +0530

    Update info in project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject spectacular "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Spec arbitrary Clojure expressions!"
+  :url "http://github.com/divs1210/spectacular"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]])
